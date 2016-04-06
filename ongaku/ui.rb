5beef63fc7500d7656c1b24d6a82261fad54eb47
osboxes.org <atrox3d@gmail.com>
2016-04-06 13:57:51 +0100

    ui.rb

---
+++ ongaku/ui.rb
@@ -0,0 +1,57 @@
+require 'yaml'
+require 'ongaku/ui/text'
+require 'ongaku/ui/gui'
+require 'ongaku/ui/web'
+
+module Ongaku
+	class Ui
+		def self.main ui
+			puts "#{self}"
+			@modifica = false
+			$archivio = Array.new
+			Ui.carica_cd
+
+			case ui
+				when 'text'
+					interfaccia = Ongaku::Text.new
+
+				when 'gui'
+					interfaccia = Ongaku::Gui.new
+
+				when 'web'
+					interfaccia = Ongaku::Web.new
+
+				else
+					puts "interfaccia non implementata"
+			end
+			interfaccia.avvia
+		end
+
+		def self.carica_cd
+			YAML::load_documents(File.open(NOMEFILE, File::CREAT)) do |cd|
+				$archivio << cd
+			end
+			$archivio.sort!
+		end
+
+		def salva_cd 
+			if @modifica 
+				open(NOMEFILE, 'w') do |file| 
+					$archivio.each { |cd| YAML::dump cd, file }
+				end
+			end
+		end
+
+		def self.aggiungi_cd tit, aut, anno
+			@modifica = true
+			nuovo_cd = Cd.new tit, aut, anno
+			$archivio << nuovo_cd
+			$archivio.sort!
+		end
+		
+		def elimina_cd index
+			@modifica = true
+			$archivio.slice! index
+		end
+	end
+end
